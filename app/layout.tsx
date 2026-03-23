aacdca41671d75d265e64dfc00b9fc094fece38d
Ankur Fadia <ankur.fadia@gmail.com>
2026-03-23 14:31:33 -0400

    fix google verification properly

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import { SITE } from "@/lib/site-config";
 export const metadata: Metadata = {
   title: SITE.siteTitle,
   description: SITE.metaDescription,
+  verification: {
+    google: "7zbnhS7OsJE6waxkAIGb9zrZQxU9xIdN4sgZyusoIec",
+  },
 };
 
 export default function RootLayout({
@@ -14,9 +17,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-  <head>
-    <meta name="google-site-verification" content="7zbnhS7OsJE6waxkAIGb9zrZQxU9xIdN4sgZyusoIec" />" />
-  </head>
       <body>{children}</body>
     </html>
   );
