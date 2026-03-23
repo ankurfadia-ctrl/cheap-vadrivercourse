af6903f101a072a265ce9ab3d7ba0764b180b362
Ankur Fadia <ankur.fadia@gmail.com>
2026-03-23 14:07:40 -0400

    google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+  <head>
+    <meta name="google-site-verification" content="<meta name="google-site-verification" content="7zbnhS7OsJE6waxkAIGb9zrZQxU9xIdN4sgZyusoIec" />" />
+  </head>
       <body>{children}</body>
     </html>
   );
